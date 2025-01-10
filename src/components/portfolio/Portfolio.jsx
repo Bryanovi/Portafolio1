4cb868bfc70133d1f3619034cb2a3aa26bc8690a
Bryanovi <boviedo4550@uta.edu.ec>
2025-01-10 03:20:09 -0500

    Actualizacion de etiquetas cada proyecto

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,54 +5,69 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "Inventory Management System - Web",
+    title: "Inventory Management - Web",
     img: "/InvControl-Web.png",
-    desc: "The project focused on inventory management within the FISEI faculty aims to streamline the tracking and administration of various assets, such as computers, cameras, and software licenses. It integrates a central management system to monitor the availability, usage, and maintenance status of these resources.. The project aims to improve resource allocation, prevent overuse or loss of assets, and enhance overall operational efficiency within the faculty.",
+    desc: "The project focused on inventory management within the FISEI faculty aims to streamline the tracking and administration of various assets, such as computers, cameras, and software licenses.",
+    participation: "I was responsible for developing the frontend of the web application using Angular.",
     link: "https://github.com/Invcontrol-Back/dasfrontend",
+    technologies: ["/angular.png", "/python.png", "/django.png"]
   },
   {
     id: 2,
-    title: "Inventory Management System - Mobile",
+    title: "Inventory Manage - Mobile",
     img: "/InvControl-Movil.png",
-    desc: "This project on inventory management within the FISEI faculty incorporates the use of QR codes to efficiently track and manage computers. By assigning unique QR codes to each computer, the system allows for quick and accurate identification, updating, and location tracking. Staff can scan the QR codes to access detailed information about the computers, such as their specifications, maintenance history, and current user assignment.",
+    desc: "This project on inventory management within the FISEI faculty incorporates the use of QR codes to efficiently track and manage computers.",
+    participation: "I was responsible for developing the frontend of the mobile application using Flutter and using the services made in Django.",
     link: "https://github.com/Invcontrol-Back/MovilFrontend",
+    technologies: ["/flutter.png", "/django.png"]
   },
   {
     id: 3,
     title: "Project Managment - Web",
     img: "https://neubox.com/blog/wp-content/uploads/2023/09/Blog_Neubox_Gestio%CC%81n-de-tareas-1_Imagen-1.webp",
-    desc: "This project management app is designed for efficient project tracking and task management. It leverages Angular to provide a dynamic and MongoDB is used for flexible and scalable data storage, making it ideal for handling large amounts of project-related information. The app includes features like user authentication, task assignments, and detailed reporting, allowing teams to track progress, manage resources, and generate insights on project performance..",
+    desc: "This project management app is designed for efficient project tracking and task management. Angular to provide a dynamic and MongoDB is used for flexible and scalable data storage. ",
+    participation: "I was responsible for developing the frontend of the web application using Angular and integrating the API endpoints for CRUD operations.",
     link: "https://github.com/Bryanovi/CONCURSO",
+    technologies: ["/angular.png", "/mongo.png"]
   },
   {
     id: 4,
     title: "Project Store Mobiles - Web",
     img: "https://tecnosoluciones.com/wp-content/uploads/2024/06/Crear-un-Sitio-Web-para-una-Tienda-de-Celulares-y-Accesorios.webp",
-    desc: "An online store developed using Angular for a dynamic, responsive user interface and MongoDB for scalable, flexible data storage. It allows users to browse products, add items to the cart, and complete purchases. Features include user authentication, product categorization, order tracking, and payment processing, all integrated seamlessly for an optimal shopping experience. The app ensures fast performance and easy management of product listings, inventory, and customer data.",
+    desc: "An online store developed using Angular for a dynamic, responsive user interface and MongoDB for scalable, flexible data storage. It allows users to browse products, add items to the cart.",
+    participation: "I participated in the development of the user interface, the implementation of the shopping cart function.",
     link: "https://github.com/Bryanovi/Service-Telephone",
+    technologies: ["/angular.png", "/mongo.png","/firebase.png"]
   },
   {
     id: 5,
     title: "Project Store Clothes - Mobile",
     img: "https://applicantes.com/wp-content/uploads/2021/06/patpat-app-ropa-ninos-barata.jpg",
-    desc: "The Mobile Clothing Store App allows users to browse a wide range of clothing products, add them to their shopping cart, and securely complete purchases. It offers features like user authentication, order tracking, and a smooth shopping experience. Built with Android Studio and Firebase, it ensures seamless navigation and secure transactions.",
+    desc: "The Mobile Clothing Store App allows users to browse a wide range of clothing products, add them to their shopping cart, and securely complete purchases. It offers features like user authentication, order tracking, and a smooth shopping experience. Built with Android Studio and Firebase.",
+    participation: "I contributed to the development of the mobile application using Android Studio, focusing on the user authentication and order tracking features.",
     link: "https://github.com/Bryanovi/ProjectStoreClothes",
+    technologies: ["/androids.png", "/firebase.png","/mongo.png"]
   },
   {
     id: 6,
-    title: "Implementation and control of the LED Lighting System of EP-EMA - Intern",
+    title: "Implementation LED Lighting System of EP-EMA - Intern",
     img: "/em2.jpg",
-    desc: "He, together with his team, focused on developing an APP implementation for the automatic activation and control of the LED Lighting System and Security System of EP-EMA, including the multiple safety features used in this implementation.",
+    desc: "He, together with his team, focused on developing an APP implementation for the automatic activation and control of the LED Lighting System.",
+    participation: "I conducted research topics and scientific articles, collecting important information on both networks and databases. I was a test manager, documentation, and user manuals.",
     link: "",
+    technologies: ["/flutter.png"]
   },
   {
     id: 7,
     title: "Research agent in Virtual Reality for the UTA - Intern",
     img: "https://invdes.com.mx/wp-content/uploads/2022/08/02-08-22-virtual-800x445.jpg",
-    desc: "I am focused on creating a virtual reality (VR) app designed to serve as a guide for individuals without medical knowledge. This app will provide an immersive learning experience, teaching users about surgical instruments and medical procedures. By leveraging VR technology, my goal is to offer accessible training that can help individuals better understand the medical field and its tools, enhancing their knowledge in a safe, interactive environment.",
+    desc: "I am focused on creating a virtual reality (VR) app designed to serve as a guide for individuals without medical knowledge. This app will provide an immersive learning experience, teaching users about surgical instruments and medical procedures. This can help individuals better understand the medical field and its tools.",
+    participation: "I conduct research on scientific articles, compiling important information. I am a manager in the area of ​​surgical instrument modeling.",
     link: "",
-  },
+    technologies: ["/unityb.png", "/blender.png"]
+  }
 ];
+
 
 const Single = ({ item }) => {
   const ref = useRef();
@@ -73,17 +88,28 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
+            <h3>Participation</h3>
+            <p>{item.participation}</p>
+            {/* Nueva sección de tecnologías */}
+            <div className="technologies">
+              {item.technologies.map((tech, index) => (
+                <img key={index} src={tech} alt="Technology" />
+              ))}
+            </div>
             {item.link && (
-  <a href={item.link} target="_blank" rel="noopener noreferrer">
-    <button>See Repository</button>
-  </a>
-)}
+              <a href={item.link} target="_blank" rel="noopener noreferrer">
+                <button>See Repository</button>
+              </a>
+            )}
+
+            
           </motion.div>
         </div>
       </div>
     </section>
   );
 };
+
 
 const Portfolio = () => {
   const ref = useRef();
